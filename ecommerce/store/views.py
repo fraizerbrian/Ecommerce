8cdcf913c3f65c9aed3cff2266780e38afcdde63
fraizerbrian <fraizerkilonzo@gmail.com>
2021-01-03 15:24:33 +0300

    added csrftoken

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
+
 # Create your views here.
 def store(request):
     products = Product.objects.all()
@@ -29,3 +32,19 @@ def checkout(request):
         items = []
     context = {'items':items,'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.data)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('ProductId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    return JsonResponse('Item was added', safe=False)
